77f3923bb5271f4766b10f44b3b43a6ff38ee776
Carlos Antonio <carlosantonioob@gmail.com>
2024-10-27 16:08:49 -0300

    criando funções
    Foi criado varias funções com retorno e sem retorno para treino, neste exércicio

--- funções.c
+++ funções.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 #include <windows.h>
 
+//struct variaveis procedimentos aritimetricos
+typedef struct{
+    int opcao,contador,n1,n2,resultado;
+
+}opAritimetrica;
+
+
+//procedimentro imprime linha pontilhada
+void MostrarLinha(){
+
+    printf("------------------------\n");
+
+}
+
+//Função de soma 2 números
 int soma(int num1, int num2){
     int total;
     total = num1+num2;
@@ -9,13 +24,14 @@ int soma(int num1, int num2){
     return total;
 }
 
+//Função multiplica 2 números
 int mult(int num1, int num2){
     int total;
     total = num1 * num2;
 
     return total;
 }
-
+//Função divide 2 números
 float divi(float num1, float num2){
     float total;
     total = num1 / num2;
@@ -23,6 +39,7 @@ float divi(float num1, float num2){
     return total;
 }
 
+//Função subtrai 2 números
 int sub(int num1, int num2){
     int total;
     total = num1 - num2;
@@ -31,6 +48,7 @@ int sub(int num1, int num2){
 
 }
 
+//procedimento fatorial
 void Fatorial(int fato, int num){
     printf("Digite o número que deseja saber o fatorial: ");
     scanf("%d",&num);
@@ -41,92 +59,142 @@ void Fatorial(int fato, int num){
         printf("%d x ",num);
     }
     printf("1 = %d\n",fato);
-
+    system("pause");
+    printf("Voltando ao menu anterior...");
+    Sleep(500);
 }
 
+//função maior e menor 2 números
+float Maior(float num1, float num2){
+    if(num1 > num2){
+        return num1;
+    }else if(num2 > num1){
+        return num2;
+    }else{
+        return num1;//se os números forem iguais retorna num 1
+    }
+}
 
-void MostrarLinha(){
+//procedimento maior e menor
+void maior(float n1, float n2){
+    float resultado;
+    printf("Digite 2 números direi qual o maior entre eles.\n");
+    printf("Digite o primeiro número: ");
+    scanf("%f",&n1);
+    getchar();
+    printf("Digite o segundo número: ");
+    scanf("%f",&n2);
+    getchar();
+    resultado = Maior(n1,n2);
+    printf("O maior número é : %.2f\n",resultado);
+    system("pause");
 
-    printf("------------------------\n");
+}
+//procedimento 4 operações aritimetricas
+void OperacaoAritimetrica(opAritimetrica aritimetrica){
+    aritimetrica.contador = 0;
+    do{
+            system("cls");
+            MostrarLinha();
+            printf("Qual operação aritimetrica deseja fazer ?\n");
+            printf("1 = SOMAR\n");
+            printf("2 = SUBTRAIR\n");
+            printf("3 = Multiplicar\n");
+            printf("4 = DIVIDIR\n");
+            printf("0 = SAIR\n");
+            printf("Digite sua opção: ");
+            scanf("%d",&aritimetrica.opcao);
+            getchar();
+            MostrarLinha();
+            if (aritimetrica.opcao != 0){
+                aritimetrica.contador++;
+                printf("Digite o primeiro número: ");
+                scanf("%d",&aritimetrica.n1);
+                getchar();
+                printf("Digite o segundo número: ");
+                scanf("%d",&aritimetrica.n2);
+                getchar();
+            }
+            switch (aritimetrica.opcao){
+                case 1:
+                    aritimetrica.resultado = soma(aritimetrica.n1,aritimetrica.n2);
+                    printf("A Soma entre %d + %d é = %d\n",aritimetrica.n1,aritimetrica.n2,aritimetrica.resultado);
+                    MostrarLinha();
+                    system("Pause");
+                    break;
+                case 2:
+                    aritimetrica.resultado = sub(aritimetrica.n1,aritimetrica.n2);
+                    printf("A subtração entre %d - %d é = %d\n",aritimetrica.n1,aritimetrica.n2,aritimetrica.resultado);
+                    MostrarLinha();
+                    system("Pause");
+                    break;
+                case 3:
+                    aritimetrica.resultado = mult(aritimetrica.n1,aritimetrica.n2);
+                    printf("A multiplicação entre %d * %d é = %d\n",aritimetrica.n1,aritimetrica.n2,aritimetrica.resultado);
+                    MostrarLinha();
+                    system("Pause");
+                    break;
+                case 4:
+                    aritimetrica.resultado = divi(aritimetrica.n1,aritimetrica.n2);
+                    printf("A divisão entre %d / %d é = %d\n",aritimetrica.n1,aritimetrica.n2,aritimetrica.resultado);
+                    MostrarLinha();
+                    system("Pause");
+                    break;
+                case 0:
+                    printf("Você fez %d operações no programa.\n",aritimetrica.contador);
+                    Sleep(500);
+                    printf("Voltando ao menu principal...");
+                    Sleep(500);
+                    default:
+                    break;
+                }
+
+
+    }while(aritimetrica.opcao != 0);
 
 }
 
 
 int main(){
     setlocale(LC_ALL,"Portuguese");
+    opAritimetrica aritimetrica;
     int fato, resultadoo;
-    int n1,n2,resultado,opcao,opcao1;
-    int contador;
-    contador = 0;
-    printf("1 = Fatorial\n");
-    printf("2 = Operações aritimetricas\n");
-    printf("Escolha sua opção:\n");
-    scanf("%d",&opcao1);
-    getchar();
-    system("cls");
-    MostrarLinha();
-    if(opcao1 == 1){
-        Fatorial(fato, resultadoo);
-    }else if(opcao1 == 2){
-        do{
+    int opcao1; // variavel swhitch case menu principal
+    int parar = 0;
+    int n1,n2;
+    while (parar == 0){
+
         system("cls");
-        printf("Digite o primeiro número: ");
-        scanf("%d",&n1);
-        getchar();
-        printf("Digite o segundo número: ");
-        scanf("%d",&n2);
-        getchar();
-        MostrarLinha();
-        printf("Qual operação aritimetrica deseja fazer com os números?\n");
-        printf("1 = SOMAR\n");
-        printf("2 = SUBTRAIR\n");
-        printf("3 = Multiplicar\n");
-        printf("4 = DIVIDIR\n");
+        printf("1 = Fatorial\n");
+        printf("2 = Operações aritimetricas\n");
+        printf("3 = Númerio maior.\n");
         printf("0 = SAIR\n");
-        printf("Digite sua opção: ");
-        scanf("%d",&opcao);
+        printf("Escolha sua opção: ");
+        scanf("%d",&opcao1);
         getchar();
+        system("cls");
         MostrarLinha();
-        if (opcao != 0){
-            contador++;
+        switch(opcao1){
+        case 1:
+            Fatorial(fato, resultadoo);
+            break;
+        case 2:
+            OperacaoAritimetrica(aritimetrica);
+            break;
+        case 3:
+            maior(n1,n2);
+            break;
+        case 0:
+            printf("Saindo do programa...\n");
+            Sleep(500);
+            parar = 1;
+            break;
+        default:
+            printf("Opção invalida");
+            Sleep(500);
+            break;
         }
-        switch (opcao){
-            case 1:
-                resultado = soma(n1,n2);
-                printf("A Soma entre %d + %d é = %d\n",n1,n2,resultado);
-                MostrarLinha();
-                break;
-            case 2:
-                resultado = sub(n1,n2);
-                printf("A subtração entre %d - %d é = %d\n",n1,n2,resultado);
-                MostrarLinha();
-                break;
-            case 3:
-                resultado = mult(n1,n2);
-                printf("A multiplicação entre %d * %d é = %d\n",n1,n2,resultado);
-                MostrarLinha();
-                break;
-            case 4:
-                resultado = divi(n1,n2);
-                printf("A divisão entre %d / %d é = %d\n",n1,n2,resultado);
-                MostrarLinha();
-                break;
-            case 0:
-                printf("Você fez %d operações no programa.\n",contador);
-                Sleep(500);
-                printf("Saindo do programa...");
-                Sleep(500);
-                default:
-                break;
-            }
 
-
-        }while(opcao != 0);
-    }else{
-        printf("Opção invalida!\n");
     }
-   
-
-
     return 0;
 }
