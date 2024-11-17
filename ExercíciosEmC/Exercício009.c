048dd0ccd2eb33bbd2497a0d0210d47bd01e5fbf
Carlos Antonio <carlosantonioob@gmail.com>
2024-11-16 22:32:54 -0300

    Exercício

---
+++ ExercíciosEmC/Exercício009.c
@@ -0,0 +1,42 @@
+/*Escreva um programa em linguagem C que solicite ao usuário a entrada de 5 números inteiros e armazene-os em um array. O programa deve:
+
+Determinar o maior número.
+Determinar o menor número.
+Calcular a média dos números.
+Exiba os resultados na tela.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    int numeros[5];
+    int maior,menor, soma = 0;
+    float media;
+
+    for(int i = 0; i < 5; i++){
+        printf("Digite o %dº número: ",i+1);
+        scanf("%d",&numeros[i]);
+        soma = soma + numeros[i];
+        if(i == 0){
+            maior = numeros[i];
+            menor = numeros[i];
+        }
+        if(numeros[i] > maior){
+            maior = numeros[i];
+        }
+        if(numeros[i] < menor){
+            menor = numeros[i];
+        }
+    }
+    media = soma / 5;
+    for (int i = 0; i < 5; i++){
+        printf("%dº digitado: %d \n",i+1,numeros[i]);
+    }
+    
+    printf("O maior número é: %d\n",maior);
+    printf("O menor número é : %d\n",menor);
+    printf("A média dos números é: %.2f\n",media);
+
+
+}
